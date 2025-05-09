d677183000b844a40a513433662650d00d1c89a2
melissalaskowski <mellaskowski12@gmail.com>
2025-05-09 17:27:44 -0400

    add title and new section

--- app/components/VideoSection.tsx
+++ app/components/VideoSection.tsx
@@ -1,28 +1,38 @@
 import { Link } from "@remix-run/react";
+import { useState } from "react";
 
 enum MusicVideoURL {
   OneThatGotAway = "https://www.youtube.com/embed/-auMY71v5cs",
-  WhatIWant = "https://www.youtube.com/embed/ysQl74R_Tog"
+  WhatIWant = "https://www.youtube.com/embed/ysQl74R_Tog",
+  SilkChiffonAtTheGreek = "https://www.youtube.com/embed/P4cw2YiOykk?si=PaAn6XgCjNhwv0lA"
 }
 
-function VideoSection(page) {
+function VideoSection(params: {page: string}) {
 
-  if (page) {
+  let mainPage = false;
+  if (params.page==='music_video') {
     // get page specific order of videos
+    mainPage = true;
   }
+
+
 
   // return default video section
     return (
+
+      
         <div className="grid grid-rows-1 grid-flow-col gap-1 mx-45 py-6 align-middle">
         <div className="flex flex-wrap justify-center gap-8">
+        
           <iframe width="520" height="245" src={MusicVideoURL.OneThatGotAway} className="rounded-lg">
           </iframe>
           <iframe width="520" height="245" src={MusicVideoURL.WhatIWant} className="rounded-lg" />
         </div>
+        { !mainPage && 
         <Link to="/content/musicVideo"
               className="flex items-center justify-center rounded-sm bg-violet-500 px-4 py-3 font-medium text-white hover:bg-violet-600  "
         > Check Out More Music Videos
-        </Link>
+        </Link> }
       </div>
     );
 }
--- app/routes/content/musicVideo.tsx
+++ app/routes/content/musicVideo.tsx
@@ -1,10 +1,30 @@
+import Header from "~/components/Header";
 import VideoSection from "~/components/VideoSection";
 
 export default function MusicVideo () {
 
+    const options = ["Music Videos", "Gayotic", "Live Performances"];
+    console.log('options', options.length);
     return (
         <div>
+            <Header />
+            <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+                <span className="block uppercase text-violet-500 drop-shadow-md">
+                  Videos
+                </span>
+              </h1>
+            <div>
+                {options.forEach((value, index) => {
+                    return(
+                               <button type="button">
+                               {options[index]}
+                           </button> );
+                })}
+     
+     </div>
+    
             <VideoSection page='music_video'/>
+
         </div>
     );
 }
