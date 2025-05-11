c2eecb0d2291f986909b3619960de3db8686f304
melissalaskowski <mellaskowski12@gmail.com>
2025-05-10 21:15:20 -0400

    redirect to gayotic content for the moment

--- app/components/MusicRadioButton.tsx
+++ app/components/MusicRadioButton.tsx
@@ -1,3 +1,4 @@
+import { redirect } from "@remix-run/node";
 
 interface ButtonProps {
     option: string,
@@ -8,9 +9,16 @@ function MusicRadioButton(ButtonProps:ButtonProps) {
 
     const options = ["Music Videos", "Gayotic", "Live Performances"];
    
+    const onClick = () => {
+        if (ButtonProps.option === options[1]) {
+            return redirect("/gayotic/thatsrank");
+        }
+        return ButtonProps.updateChoice(ButtonProps.option);
+    }
+
     return (
             <button 
-                onClick={() => ButtonProps.updateChoice(ButtonProps.option)}
+                onClick={onClick}
                 
                 className="justify-center rounded-md bg-violet-500 px-4 py-3 font-medium text-white hover:bg-violet-600"
             >
