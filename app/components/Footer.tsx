02b29866af3325c9b3b8d9d9c3e9c95c534af7cf
melissalaskowski <mellaskowski12@gmail.com>
2025-08-01 18:04:57 -0400

    add design logo in the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -20,15 +20,11 @@ function Footer() {
     
 
     return (
-        <div className="bg-violet-300 h-1/6 px-15">
-      
-            About.
-            Contact.
-
-            Sources.
-            References.
-            Content Attributions.
-      
+        <div className="flex bg-violet-300 h-1/7 px-15 justify-center">
+            <img src={"images/design_logo/Laz_designs.png"}
+                alt="Laz designs logo with text 'Laz designs'" 
+                className="flex max-h-40"
+                />
 
         </div>
     );
